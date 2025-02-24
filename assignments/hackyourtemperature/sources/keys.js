840fe437907489541f6f5d952ee5e36192feea66
Samira <ahmadi.samira6761@gmail.com>
2025-02-24 19:49:15 +0100

    Samira-node-w2

--- assignments/hackyourtemperature/sources/keys.js
+++ assignments/hackyourtemperature/sources/keys.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export default { BASE_URL: "https://api.openweathermap.org/data/2.5", // Base URL for openWeatherMap API
-API_KEY: process.env.API_KEY,
+export default { 
+    BASE_URL: process.env.BASE_URL,
+    API_KEY: process.env.API_KEY,
 };
