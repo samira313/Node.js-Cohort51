af5462836247dd6a8976c22060329a1c013b9a59
Samira <ahmadi.samira6761@gmail.com>
2025-03-07 20:43:32 +0100

    New update

--- assignments/hackyourtemperature2/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-
-const PORT = 3000;
-
-// Start the server on the specified port
-app.listen(PORT, () => {
-    console.log(`Server is runnig on port ${PORT}`);
-});
--- assignments/hackyourtemperature2/sources/keys.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export default { 
-    BASE_URL:process.env.BASE_URL,
-    API_KEY:process.env.API_KEY,
-};
